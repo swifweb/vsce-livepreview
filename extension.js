c01fb7691417dee3651061009639952c778e3ce0
Mihael Isaev <isaev.mihael@gmail.com>
2021-03-13 05:43:52 +0400

    👨‍🔧 Removed redundant import

--- extension.js
+++ extension.js
@@ -1,11 +1,5 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const { readBuilderProgram } = require('typescript');
 const vscode = require('vscode');
 const fs = require('fs');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
